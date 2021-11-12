e5907fb3a8692dafa925e4fea072a20514b09d29
gauravbioct30 <gauravbioct30@gmail.com>
2021-11-12 17:39:55 +0530

    javascript notes

---
+++ 07-NOV-2021/02-nov-2021/jd/JS/06-NOV-2021/script.js
@@ -0,0 +1,22 @@
+document.write("Hello world")
+document.write("heyyyy")
+document.write("what is uppp")
+document.write("what is your code")
+document.write(12343)
+document.write("select true / false")
+document.write("true")
+
+var gaurav = 'BOARD INFINITY' // Variable
+
+document.write(gaurav)
+
+// alert('you have chosen alert')
+// alert('this is how alert works')
+
+// document.getElementById("name").innerHTML ="My name us ram"
+
+console.log('my name is gaurav');
+console.log('123');
+console.log('true');
+console.log(gaurav);
+console.warn('warning');
