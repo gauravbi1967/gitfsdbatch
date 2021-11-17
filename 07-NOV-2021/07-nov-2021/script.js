9e99969980a67ae82f80745a2489eb1484c4aa09
gauravbioct30 <gauravbioct30@gmail.com>
2021-11-17 18:01:43 +0530

    condition and switch

---
+++ 07-NOV-2021/07-nov-2021/script.js
@@ -0,0 +1,63 @@
+let username = 'gaurav';
+let marks = 30;
+let isMarried = false ;
+let hobbies = ['cooding','cooking']
+
+
+console.log(typeof hobbies);
+
+                // arithmethic operator  
+
+let n1 =20;
+let n2 =10;
+
+var result = n1+n2; // addition 
+console.log(result)
+
+result = n1-n2; // substraction 
+console.log(result);
+
+result = n1*n2; // multiplication 
+console.log(result);
+
+result = n1%n2; // modulus
+console.log(result);
+
+result = n1**n2; // exponential
+console.log(result);
+
+result = n1/n2; // division 
+console.log(result);
+
+// Post increment 
+  console.log(n1);
+  n1++;  // 20++ =20+1 = 21 
+  console.log(n1);
+
+// Pre increment 
+  ++n1; //20++ = 20+1 = 22
+  console.log(n1);
+
+  for(let i=0; i <= 10; i++){
+      console.log(i);
+      }
+
+// Post decrement 
+   console.log(n1);
+   n1--;  // 22-- =22-1 = 21
+   console.log(n1);
+
+// Pre decrement 
+   --n1; //--21 = 21-1 = 20
+   console.log(n1);
+
+                 // assignment operator 
+   
+    n1=30;
+    n2=40;
+
+    let res = n1+=n2
+    console.log(res);
+
+    res = n1-=n2
+    console.log(res);
